225ca2420d7df0ea25e771ceaea99f3a7d78a7dc
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-07-25 20:05:03 -0600

    ♻️ refactor: update App component layout and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
+import HookJR from './components/HookJR';
 
 function App() {
   return (
-    <div className="w-screen h-screen bg-blue-600">
-      <Canvas>
-        <OrbitControls />
+    <div className="w-screen h-screen bg-gradient-to-b from-blue-900 to-indigo-950 m-0 p-0 overflow-hidden">
+      <Canvas
+        camera={{ position: [0, 1.8, 4], fov: 50 }}
+      >
+        
+        <OrbitControls enablePan={false} enableRotate={true} enableZoom={false} />
         <ambientLight intensity={0.5} />
         <directionalLight position={[2, 2, 2]} intensity={1} />
-        {/* Aquí irá Hook JR */}
+        <HookJR />
       </Canvas>
     </div>
   );
